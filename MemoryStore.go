6b9da2a69cf6a6cbd734217ae63b6dffca1211ff
Chara-X <1719552189@qq.com>
2024-05-02 07:08:50 +0800

    Add Delete method

--- MemoryStore.go
+++ MemoryStore.go
@@ -8,6 +8,10 @@ import (
 type MemoryStore[T any] struct{ store *sync.Map }
 
 func NewMemoryStore[T any]() MemoryStore[T] { return MemoryStore[T]{new(sync.Map)} }
+func (s *MemoryStore[T]) Store(key string, value T, duration time.Duration) {
+	s.Delete(key)
+	s.store.Store(key, entry[T]{value, time.AfterFunc(duration, func() { s.store.Delete(key) }), duration})
+}
 func (s *MemoryStore[T]) Load(key string) (T, bool) {
 	if v, ok := s.store.Load(key); ok {
 		var entry = v.(entry[T])
@@ -16,12 +20,11 @@ func (s *MemoryStore[T]) Load(key string) (T, bool) {
 	}
 	return *new(T), false
 }
-func (s *MemoryStore[T]) Store(key string, value T, duration time.Duration) {
+func (s *MemoryStore[T]) Delete(key string) {
 	if v, ok := s.store.Load(key); ok {
-		var entry = v.(entry[T])
-		entry.timer.Stop()
+		v.(entry[T]).timer.Stop()
 	}
-	s.store.Store(key, entry[T]{value, time.AfterFunc(duration, func() { s.store.Delete(key) }), duration})
+	s.store.Delete(key)
 }
 
 type entry[T any] struct {
